15fbc51de87073c54d9ecee6a8ac07e1d661f7ca
omn14 <sirole@gmail.com>
2025-10-19 19:50:13 +0200

    remove function simulate_battle_ranged

--- battleFunctions.py
+++ battleFunctions.py
@@ -56,8 +56,6 @@ def simulate_battle(unit1, unit2,charge: bool):
         unit1.model.charging = True
         for rule in unit1.model.special_rules:
             if rule.get('charge'):
-                #attacks = (int(unit1.model.characteristics.get('A', 0)) + 1) * unit1.files #front rank attacks
-                #print(unit1.model.special_rules[1]['charge'](unit1.model))
                 rule['charge'](unit1.model)
             else:
                 attacks = int(unit1.model.characteristics.get('A', 0)) * unit1.files #front rank attacks
@@ -95,48 +93,4 @@ def simulate_battle(unit1, unit2,charge: bool):
 
     return attacks,total_hits, suffered_wounds,  saves_made, total_wounds
 
-def simulate_battle_ranged(unit1, unit2, charge: bool):
 
-    # how many attacks
-    if charge:
-        unit1.model.charging = True
-        for rule in unit1.model.special_rules:
-            if rule.get('charge'):
-                #attacks = (int(unit1.model.characteristics.get('A', 0)) + 1) * unit1.files #front rank attacks
-                #print(unit1.model.special_rules[1]['charge'](unit1.model))
-                rule['charge'](unit1.model)
-            else:
-                attacks = int(unit1.model.characteristics.get('A', 0)) * unit1.files #front rank attacks
-        attacks = int(unit1.model.characteristics.get('A', 0)) * unit1.files
-    else: #defends
-        attacks = int(unit1.model.characteristics.get('A', 0)) * unit1.files #front rank attacks
-        if attacks >= int(unit1.model.characteristics.get('A', 0)) *unit1.nmodels: 
-            attacks = int(unit1.model.characteristics.get('A', 0)) *unit1.nmodels # cannot attack more than you have models in front rank
-        elif unit1.nmodels % unit1.files > 0: # uncomplete second rank
-            attacks +=   (unit1.nmodels % unit1.files) # only one attack if not in base contact
-        else:
-            attacks += unit1.files # full second rank
-    attacks1 = attacks 
-    print(f"Total attacks by {unit1.name} on {unit2.name}: {attacks1}")
-    total_hits = 0
-    total_wounds = 0
-    suffered_wounds = 0
-    saves_made = 0
-    for i in range(attacks1):
-        hit,wound = simulate_attack_ranged(unit1.model, unit2.model)
-        if hit:
-            total_hits += 1
-        if wound:
-            total_wounds += 1
-            suffered_wounds += 1
-        if wound:
-            print(unit1.model.AP)
-            if check_armor_save(unit2.model,unit2.model.armor_save, unit1.model.AP):
-                saves_made += 1
-                total_wounds -= 1
-    
-    unit1.model.reset_characteristics()
-    unit2.model.reset_characteristics()
-    
-
-    return attacks,total_hits, suffered_wounds,  saves_made, total_wounds
