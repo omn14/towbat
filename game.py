96bccdcc007c9d24c990aa86e74b74dff12b7899
omn14 <sirole@gmail.com>
2026-09-08 20:54:44 +0200

    new starting army

--- game.py
+++ game.py
@@ -331,9 +331,9 @@ class MyApp(ShowBase):
 
         #self.load_player1_army("strategy_armies/hammer_and_anvil.json")
         #self.p1army="strategy_armies/orc_and_goblin_horde.json"
-        self.p1army="strategy_armies/Bm_army.json"
+        self.p1army="strategy_armies/my_army_he.json"
         self.load_player1_army(self.p1army)
-        self.load_player2_army("strategy_armies/vampire_counts_legion.json")
+        self.load_player2_army("strategy_armies/my_arm_chaosy.json")
 
 
         self.unitToMove=self.player1Units[0]
